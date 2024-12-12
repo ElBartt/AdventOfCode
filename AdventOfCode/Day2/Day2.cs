68f9d924e96555408fb954bba57e5a6e63b9cde1
ElBartt <arnauld.alex@gmail.com>
2024-12-11 21:59:15 -0500

    Day 2 part 2

--- AdventOfCode/Day2/Day2.cs
+++ AdventOfCode/Day2/Day2.cs
@@ -2,8 +2,10 @@ namespace AdventOfCode
 {
     public class Day2 : IDay
     {
-        List<Report> m_Reports = new List<Report>();
-        int m_SafeReportNumber = 0;
+        private const int ms_part = 2;
+
+        private List<Report> m_Reports = new List<Report>();
+        private int m_SafeReportNumber = 0;
 
         public void DisplayResult()
         {
@@ -13,7 +15,19 @@ namespace AdventOfCode
         public void Run()
         {
             ReadInput();
-            ComputeSafeReports();
+
+            switch (ms_part)
+            {
+                case 1:
+                    ComputeSafeReports();
+                    break;
+                case 2:
+                    ComputeSafeReportsWithDampener();
+                    break;
+                default:
+                    Console.WriteLine("¯\\_(--')_/¯");
+                    break;
+            }
         }
 
         private void ReadInput()
@@ -31,13 +45,27 @@ namespace AdventOfCode
         {
             foreach (Report report in m_Reports)
             {
-                m_SafeReportNumber += report.IsSafe() ? 1 : 0;
+                m_SafeReportNumber += report.IsSafe().safe ? 1 : 0;
+            }
+        }
+
+        private void ComputeSafeReportsWithDampener()
+        {
+            foreach (Report report in m_Reports)
+            {
+                m_SafeReportNumber += report.IsSafeWithDampener() ? 1 : 0;
             }
         }
 
         private class Report
         {
             List<int> m_Levels = new List<int>();
+
+            public struct SafetyReport
+            {
+                public int faultyIndex;
+                public bool safe;
+            }
 
             public Report(string reportLine)
             {
@@ -47,26 +75,52 @@ namespace AdventOfCode
                 }
             }
 
-            public bool IsSafe()
+            public Report(List<int> levels)
+            {
+                m_Levels = levels;
+            }
+
+            public SafetyReport IsSafe()
             {
                 bool isIncreasing = m_Levels[1] > m_Levels[0];
 
                 for (int i = 0; i < m_Levels.Count - 1; i++)
                 {
                     int levelDifference = m_Levels[i] - m_Levels[i + 1];
-                    
+
                     if (Math.Abs(levelDifference) is < 1 or > 3 || levelDifference == 0)
                     {
-                        return false;
+                        return new SafetyReport { faultyIndex = i, safe = false };
                     }
 
                     if ((isIncreasing && levelDifference > 0) || (isIncreasing == false && levelDifference < 0))
                     {
-                        return false;
+                        return new SafetyReport { faultyIndex = i, safe = false };
                     }
                 }
 
-                return true;
+                return new SafetyReport { faultyIndex = -1, safe = true };
+            }
+
+            public bool IsSafeWithDampener()
+            {
+                var safetyCheck = this.IsSafe();
+                if (safetyCheck.safe)
+                    return true;
+
+                int faultyIndex = safetyCheck.faultyIndex;
+                List<int> modifiedLevels;
+
+                for (int i = Math.Max(0, faultyIndex - 1); i <= Math.Min(faultyIndex + 1, m_Levels.Count - 1); i++)
+                {
+                    modifiedLevels = new List<int>(m_Levels);
+                    modifiedLevels.RemoveAt(i);
+                    Report modifiedReport = new Report(modifiedLevels);
+                    if (modifiedReport.IsSafe().safe)
+                        return true;
+                }
+
+                return false;
             }
         }
     }
